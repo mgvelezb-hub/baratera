0c514c85bed31941cb3161004fba80993584f56b
mgvelezb-hub <mg.velezb@gmail.com>
2026-06-11 16:27:18 -0600

    REsponsiva ticket:Final

--- src/app/venta/TicketPrint.tsx
+++ src/app/venta/TicketPrint.tsx
@@ -331,6 +331,7 @@ export default function TicketPrint({ items, total, payment, hora, onClose, onNu
             <Divider />
             <p style={{ textAlign: 'center', fontSize: '11px', marginBottom: '2px' }}>{negocio.footer1}</p>
             <p style={{ textAlign: 'center', fontSize: '11px' }}>{negocio.footer2}</p>
+            <p style={{ textAlign: 'center', fontSize: '11px' }}>{negocio.footer3}</p>
 
           </div>
         </div>
