40b24cd9fb4b3805c4dd6232e9f80a16f16275e9
mgvelezb-hub <mg.velezb@gmail.com>
2026-06-11 19:03:11 -0600

    Ajuste Ticket: Prueba

--- src/app/venta/TicketPrint.tsx
+++ src/app/venta/TicketPrint.tsx
@@ -356,16 +356,15 @@ export default function TicketPrint({ items, total, payment, hora, onClose, onNu
                 <span style={{ whiteSpace: 'nowrap' }}>{formatMXN(payment.cambio)}</span>
               </Row>
             )}
-
-            <Divider />
-            <p style={{ textAlign: 'center', fontSize: '11px', marginBottom: '2px' }}>{negocio.footer1}</p>
-            <p style={{ textAlign: 'center', fontSize: '11px' }}>{negocio.footer2}</p>
-
-            <Divider />
-            <Divider />
-
+            <Row>
+              <Divider />
+              <p style={{ textAlign: 'center', fontSize: '11px', marginBottom: '2px' }}>{negocio.footer1}</p>
+              <p style={{ textAlign: 'center', fontSize: '11px' }}>{negocio.footer2}</p>
+            </Row> 
+            
+            <Row>
             <p style={{ textAlign: 'center', fontSize: '10px' }}>{negocio.footer3}</p>
-
+            </Row>
           </div>
         </div>
 
