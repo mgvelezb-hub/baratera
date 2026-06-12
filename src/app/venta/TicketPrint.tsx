5438e377ca9414493ea9913d3ff005f6c61a7454
mgvelezb-hub <mg.velezb@gmail.com>
2026-06-11 18:57:55 -0600

    Ajuste Ticket

--- src/app/venta/TicketPrint.tsx
+++ src/app/venta/TicketPrint.tsx
@@ -360,6 +360,10 @@ export default function TicketPrint({ items, total, payment, hora, onClose, onNu
             <Divider />
             <p style={{ textAlign: 'center', fontSize: '11px', marginBottom: '2px' }}>{negocio.footer1}</p>
             <p style={{ textAlign: 'center', fontSize: '11px' }}>{negocio.footer2}</p>
+
+            <Divider />
+            <Divider />
+
             <p style={{ textAlign: 'center', fontSize: '10px' }}>{negocio.footer3}</p>
 
           </div>
